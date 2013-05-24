3060f084c16381a7bb41385c2809bf79f95b038a
Jim Cowart <me@ifandelse.com>
2013-05-24 13:23:15 -0500

    Finalized Demo

--- Note-Taker-Demo/scripts/app/main.js
+++ Note-Taker-Demo/scripts/app/main.js
@@ -1,7 +1,7 @@
 var app = (function () {
     'use strict';
 
-    // global error handling boilerplate
+    // global error handling
     var showAlert = function(message, title, callback) {
         navigator.notification.alert(message, callback || function () {
         }, title, 'OK');
@@ -15,7 +15,7 @@ var app = (function () {
         showAlert(message, 'Error occured');
         return true;
     });
-    // To keep me from being an idiot and backspace-key-ing away from the app.
+
     var onBackKeyDown = function(e) {
         e.preventDefault();
         navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
@@ -34,7 +34,6 @@ var app = (function () {
 
     document.addEventListener("deviceready", onDeviceReady, false);
 
-    // Standard Everlive app (generated) boilerplate
     var applicationSettings = {
         emptyGuid: '00000000-0000-0000-0000-000000000000',
         apiKey: 'gz7JWORqkS4PDlps'
@@ -44,10 +43,253 @@ var app = (function () {
     var el = new Everlive({
         apiKey: applicationSettings.apiKey
     });
-    
+
+    var AppHelper = {
+        resolveProfilePictureUrl: function (id) {
+            if (id && id !== applicationSettings.emptyGuid) {
+                return el.Files.getDownloadUrl(id);
+            }
+            else {
+                return 'styles/images/avatar.png';
+            }
+        },
+        logout: function () {
+            return el.Users.logout();
+        }
+    };
+
     var mobileApp = new kendo.mobile.Application(document.body, { transition: 'slide', layout: 'mobile-tabstrip' });
-    
+
+    var usersModel = (function () {
+        var currentUser = kendo.observable({ data: null });
+        var usersData;
+        var loadUsers = function () {
+            return el.Users.currentUser()
+            .then(function (data) {
+                var currentUserData = data.result;
+                currentUserData.PictureUrl = AppHelper.resolveProfilePictureUrl(currentUserData.Picture);
+                currentUser.set('data', currentUserData);
+                return el.Users.get();
+            })
+            .then(function (data) {
+                usersData = new kendo.data.ObservableArray(data.result);
+            })
+            .then(null,
+                  function (err) {
+                      showError(err.message);
+                  }
+            );
+        };
+        return {
+            load: loadUsers,
+            users: function () {
+                return usersData;
+            },
+            currentUser: currentUser
+        };
+    }());
+
+    var loginViewModel = (function () {
+        var login = function () {
+            var username = $('#loginUsername').val();
+            var password = $('#loginPassword').val();
+
+            el.Users.login(username, password)
+            .then(function () {
+                return usersModel.load();
+            })
+            .then(function () {
+                mobileApp.navigate('views/notesView.html');
+            })
+            .then(null,
+                  function (err) {
+                      showError(err.message);
+                  }
+            );
+        };
+        return {
+            login: login
+        };
+    }());
+
+    var signupViewModel = (function () {
+        var dataSource;
+        var signup = function () {
+            el.Users.register(
+                dataSource.Username,
+                dataSource.Password,
+                dataSource)
+            .then(function () {
+                showAlert("Registration successful");
+                mobileApp.navigate('#welcome');
+            },
+                  function (err) {
+                      showError(err.message);
+                  }
+            );
+        };
+        var show = function () {
+            dataSource = kendo.observable({
+                Username: '',
+                Password: '',
+                DisplayName: '',
+                Email: ''
+            });
+            kendo.bind($('#signup-form'), dataSource, kendo.mobile.ui);
+        };
+        return {
+            show: show,
+            signup: signup
+        };
+    }());
+
+    var notesModel = (function () {
+        var noteModel = {
+            id: 'Id',
+            fields: {
+                Title: {
+                    field: 'Title',
+                    defaultValue: ''
+                },
+                Text: {
+                    field: 'Text',
+                    defaultValue: ''
+                },
+                CreatedAt: {
+                    field: 'CreatedAt',
+                    defaultValue: new Date()
+                },
+                UserId: {
+                    field: 'UserId',
+                    defaultValue: ''
+                }
+            },
+            CreatedAtFormatted: function () {
+                return moment(this.get('CreatedAt')).calendar();
+            },
+            User: function () {
+                var userId = this.get('UserId');
+                var user = $.grep(usersModel.users(), function (e) {
+                    return e.Id === userId;
+                })[0];
+                return {
+                    DisplayName: user ? user.DisplayName : 'Anonymous',
+                };
+            }
+        };
+        var notesDataSource = new kendo.data.DataSource({
+            type: 'everlive',
+            schema: {
+                model: noteModel
+            },
+            transport: {
+                typeName: 'Notes'
+            },
+            change: function (e) {
+                if (e.items && e.items.length > 0) {
+                    $('#no-notes-span').hide();
+                }
+                else {
+                    $('#no-notes-span').show();
+                }
+            },
+            sort: { field: 'CreatedAt', dir: 'desc' }
+        });
+        return {
+            notes: notesDataSource
+        };
+    }());
+
+    // notes view model
+    var notesViewModel = (function () {
+        var noteSelected = function (e) {
+            mobileApp.navigate('views/noteView.html?uid=' + e.data.uid);
+        };
+        var navigateHome = function () {
+            mobileApp.navigate('#welcome');
+        };
+        var logout = function () {
+            AppHelper.logout()
+            .then(navigateHome, function (err) {
+                showError(err.message);
+                navigateHome();
+            });
+        };
+        return {
+            notes: notesModel.notes,
+            noteSelected: noteSelected,
+            logout: logout
+        };
+    }());
+
+    // note details view model
+    var noteViewModel = (function () {
+        return {
+            show: function (e) {
+                var note = notesModel.notes.getByUid(e.view.params.uid);
+                kendo.bind(e.view.element, note, kendo.mobile.ui);
+            }
+        };
+    }());
+
+    // add note view model
+    var addNoteViewModel = (function () {
+        var $newNote;
+        var $newNoteTitle;
+        var validator;
+        var noteInProgress;
+        var init = function () {
+            notesModel.notes.bind('error', function(resp) {
+                var msg;
+                notesModel.notes.unbind('sync');
+                if(notesModel.notes.hasChanges()) {
+                    notesModel.notes.cancelChanges(noteInProgress);
+                }
+                try {
+                    msg = JSON.parse(resp.xhr.responseText).message;
+                } catch(ex) {
+                    msg = "An unknown error has occurred.";
+                }
+                navigator.notification.alert(msg, function() {}, "Error");
+            });
+            validator = $('#enterNote').kendoValidator().data("kendoValidator");
+            $newNote = $('#newNote');
+            $newNoteTitle = $('#newNoteTitle');
+        };
+        var show = function () {
+            $newNote.val('');
+            validator.hideMessages();
+        };
+        var syncAction = function () {
+            noteInProgress = undefined;
+            mobileApp.navigate('#:back');
+        };
+        var saveNote = function () {
+            if (validator.validate()) {
+                var notes = notesModel.notes;
+                noteInProgress = notes.add();
+                noteInProgress.Text = $newNote.val();
+                noteInProgress.Title = $newNoteTitle.val();
+                noteInProgress.UserId = usersModel.currentUser.get('data').Id;
+                notes.one('sync', syncAction);
+                notes.sync();
+            }
+        };
+        return {
+            init: init,
+            show: show,
+            me: usersModel.currentUser,
+            saveNote: saveNote
+        };
+    }());
+
     return {
-        viewModels: {}
-    }
+        viewModels: {
+            login: loginViewModel,
+            signup: signupViewModel,
+            notes: notesViewModel,
+            note: noteViewModel,
+            addNote: addNoteViewModel
+        }
+    };
 }());
